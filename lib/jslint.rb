e5f73e38e24243e644fe94103cd3fb97f4ce7a43
ragaskar <ragaskar@gmail.com>
2013-06-20 07:11:34 -0700

    Load Railtie if Rails version is >= 3

--- lib/jslint.rb
+++ lib/jslint.rb
@@ -2,6 +2,6 @@ require 'jslint/errors'
 require 'jslint/utils'
 require 'jslint/lint'
 
-if defined?(Rails) && Rails::VERSION::MAJOR == 3
+if defined?(Rails) && Rails::VERSION::MAJOR >= 3
   require 'jslint/railtie'
 end
